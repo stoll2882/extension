17181938a52816dcddcdca592e39d7b99cf5b051
Sam Toll <sam.x.toll@gmail.com>
2022-02-09 15:25:58 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import {Container, Form, Button, Navbar} from "react-bootstrap";
 import { login, register, loadUser } from './actions/auth';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" >
+      
+      <Navbar className="App-header"> 
+          <img src="./wave.png" />
+          <h1>MyRead Curated Blog</h1>
+      </Navbar>
+      
+      <h2>Curate a new post - while you're browsing!</h2>
+
+      <div>
+        <form>
+          <label for="title">Title</label>
+          <input type="text" id="title" name="title" placeholder="Enter title of article..." />
+
+          <label for="description">Description</label>
+          <textarea type="text" id="desc" name="description" placeholder="Enter article description..."></textarea>
+
+          <label for="link">Article URL</label>
+          <input type="url" id="link" name="link" placeholder="Enter article URL..."></input>
+
+          <label for="category">Category</label>
+            <select id="category" name="category" />
+        </form>
+      </div>
+
+      <Button className="rounded-pill" type = "primary">Save Post</Button>
+
+    </Container>
   );
 }
 
@@ -35,5 +48,4 @@ export default connect(mapStateToProps, {
   login,
   register,
   loadUser,
-  setAlert,
 })(App);
--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { fetchUrl } from 'fetch';
+// import { fetchUrl } from 'fetch';
 import {
     REGISTER_FAIL,
     REGISTER_SUCCESS,
@@ -42,7 +42,7 @@ export const loadUser = () => async (dispatch) => {
     }
   
     try {
-      const res = await axios.get(getDevPrefix() + '/api/auth');
+      const res = await axios.get('http://localhost:5000/api/auth');
   
       dispatch({
         type: USER_LOADED,
@@ -71,14 +71,9 @@ export const register = (
       },
     };
     var body = JSON.stringify({ firstName, lastName, alias, email, password, phoneNumber, idNum });
-    // if (idNum) {
-    //   body = JSON.stringify({ firstName, lastName, alias, email, password, phoneNumber, idNum });
-    // } else {
-    //   body = JSON.stringify({ firstName, lastName, alias, email, password, phoneNumber });
-    // }
   
     try {
-      const res = await axios.post(getDevPrefix() + '/api/users', body, config);
+      const res = await axios.post('http://localhost:5000/api/users', body, config);
   
       dispatch({
         type: REGISTER_SUCCESS,
@@ -104,8 +99,6 @@ export const register = (
 export const login = (email, password) => async (dispatch) => {
 // export async function login(email, password) {
     var body = JSON.stringify({ email: email, password: password });
-    // body = { email: email, password: password };
-
     const config = {
       headers: {
         'Content-Type': 'application/json',
@@ -113,8 +106,7 @@ export const login = (email, password) => async (dispatch) => {
     };
   
     try {
-      const res = await axios.post(getDevPrefix() + '/api/auth', body, config);
-      // const res = await axios.post('/api/auth', body);
+      const res = await axios.post('http://localhost:5000/api/auth', body, config);
       console.log(res);
       dispatch({
         type: LOGIN_SUCCESS,
@@ -123,13 +115,6 @@ export const login = (email, password) => async (dispatch) => {
       dispatch(loadUser());
       dispatch(setAlert("Successful Login 🎉🎉🎉", 'success'));
     } catch (err) {
-      // const errors = err.response.data.errors;
-      // // return errors;
-  
-      // if (errors) {
-      //   errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-      // }
-  
       dispatch({
         type: LOGIN_FAIL,
       });
@@ -158,84 +143,8 @@ export const addCategory = (category) => async (dispatch) => {
     }
 };
 
-// Update a User's Bio
-export const updateBio = (bio) => async (dispatch) => {
-  var body = JSON.stringify({ bio: bio });
-
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  try {
-    const res = await axios.put(getDevPrefix() + '/api/users/bio', body, config);
-    // const res = await axios.post('/api/auth', body);
-    console.log(res);
-
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-// Get Posts
-export const getAllUsers = () => async (dispatch) => {
-  try {
-    const res = await axios.get(getDevPrefix() + '/api/auth/all');
-    return res.data;
-  } catch (err) {
-    console.log(err.msg);
-  }
-};
-
 export const logout = () => async (dispatch) => {
   dispatch({
     type: LOGOUT,
   });
-};
-
-export const uploadProfilePicture = (file) => async (dispatch) => {
-  try {
-    console.log("in profile upload")
-    console.log(file)
-    var result = await axios.get(getDevPrefix() + '/api/image');
-    console.log(result.data);
-    const response = await fetch(result.data, {
-      method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-      'Cache-Control': 'no-store max-age=0',
-      'Content-Type': file.type,
-      'x-ms-date': new Date().toUTCString(),
-      'x-ms-version': '2020-04-08',
-      'x-ms-blob-type': 'BlockBlob'
-      },
-      body: file, // body data type must match "Content-Type" header
-    });
-    await axios.post(getDevPrefix() + '/api/image');
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const uploadPostPicture = (file, postId) => async (dispatch) => {
-  try {
-    console.log("in post upload")
-    console.log(file)
-    var result = await axios.get(getDevPrefix() + '/api/postimage/' + postId);
-    console.log(result.data);
-    const response = await fetch(result.data, {
-      method: 'PUT', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-      'Cache-Control': 'no-store max-age=0',
-      'Content-Type': file.type,
-      'x-ms-date': new Date().toUTCString(),
-      'x-ms-version': '2020-04-08',
-      'x-ms-blob-type': 'BlockBlob'
-      },
-      body: file, // body data type must match "Content-Type" header
-    });
-    await axios.post(getDevPrefix() + '/api/image/' + postId);
-  } catch (error) {
-    console.log(error);
-  }
 };
