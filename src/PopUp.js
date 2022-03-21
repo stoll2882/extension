02751149f73a099033d20a7ef1b6d00312dbef90
Cole Thorpen <thorpencole@gmail.com>
2022-03-20 19:10:43 -0700

    can get communication

--- src/PopUp.js
+++ src/PopUp.js
@@ -6,15 +6,32 @@ import "./App.css";
 const PopUp = ({ idMessage }) => {
   // create state `open` as false
   const [open, setOpen] = useState(false);
+  // const [popupWindow, setPopupWindow] = useState();
 
   const openPopupHandler = () => {
-    var popupWindow = window.open("https://my-read-08.herokuapp.com/extension-login","mywindow","menubar=1,resizable=1,width=600,height=350");
-    
+    window.open("http://localhost:3000/extension-login","mywindow","menubar=1,resizable=1,width=600,height=350");
+    // window.open("https://my-read-08.herokuapp.com/extension-login","mywindow","menubar=1,resizable=1,width=600,height=350");
+    console.log("opened popup");
+
+    chrome.runtime.onMessageExternal.addListener(
+      function(request, sender, sendResponse) {
+        if (sender.url !== "http://localhost:3000/extension-login")
+          return;
+        if (request.openUrlInEditor)
+          // openUrl(request.openUrlInEditor);
+          console.log("request: ", request)
+      });
+
   }
+
+
+
   
   const closePopupHandler = () => {
     setOpen(false);
   }
+
+
 
   return (
     <>
