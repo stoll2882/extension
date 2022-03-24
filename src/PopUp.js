b2fc7c9d448ba212882e1f4aa0932653e127ca09
Cole Thorpen <thorpencole@gmail.com>
2022-03-24 15:03:20 -0700

    heroku

--- src/PopUp.js
+++ src/PopUp.js
@@ -9,15 +9,15 @@ import "./App.css";
 const PopUp = ({ login }) => {
 
   const openPopupHandler = () => {
-    window.open("http://localhost:3000/extension-login","mywindow","menubar=1,resizable=1,width=600,height=350");
-    // window.open("https://myreaddev-ext-logins-duhpcboow.herokuapp.com/extension-login","mywindow","menubar=1,resizable=1,width=600,height=350");
+    // window.open("http://localhost:3000/extension-login","mywindow","menubar=1,resizable=1,width=600,height=350");
+    window.open("https://myreaddev-ext-logins-duhpcboow.herokuapp.com/extension-login","mywindow","menubar=1,resizable=1,width=600,height=350");
     console.log("opened popup");
 
     chrome.runtime.onMessageExternal.addListener(
       function(request, sender, sendResponse) {
         // if (sender.url !== "https://my-read-08.herokuapp.com/extension-login")
-        if (sender.url !== "http://localhost:3000/extension-login")
-        // if (sender.url !== "https://myreaddev-ext-logins-duhpcboow.herokuapp.com/extension-login")
+        // if (sender.url !== "http://localhost:3000/extension-login")
+        if (sender.url !== "https://myreaddev-ext-logins-duhpcboow.herokuapp.com/extension-login")
           return;
         if (request.openUrlInEditorEmail && request.openUrlInEditorPword)
           // console.log("request: ", request);
